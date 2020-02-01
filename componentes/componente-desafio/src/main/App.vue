569bbaef779a9d990d3422273ef712b921e6216c
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-02-01 19:54:52 -0300

    finalização de componentização introdução com vue

---
+++ componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Menu />
+    <app-toolbar />
+    <app-carousel />
+    <app-footer />
+  </v-app>
+</template>
+<!-- @ parte do src -->
+<script>
+import Footer from "@/components/template/Footer";
+import Carousel from "@/components/widgets/Carousel";
+import Toolbar from "@/components/template/Toolbar";
+import Menu from "@/components/template/Menu";
+// Padrão nomenclatura de components
+export default {
+  components: {
+    appFooter: Footer,
+    "app-carousel": Carousel,
+    appToolbar: Toolbar,
+    Menu
+  }
+};
+</script>
---
+++ componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="contador">
+    <span>{{contador}}</span>
+    <button @click="subtrair">-</button>
+    <button @click="adicionar">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    };
+  },
+  methods: {
+    adicionar() {
+      this.contador++;
+    },
+    subtrair() {
+      this.contador > 0 ? this.contador-- : 0;
+    }
+  }
+};
+</script>
+
+  <!--Os escopos dos componentes pais não passam para os filhos,indiretos apenas diretos-->
+
+<style scoped>
+#contador span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 5px 25px;
+}
+
+#contador button {
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  background-color: coral;
+  color: #fff;
+  margin-left: 10px;
+  outline: none;
+}
+</style>
