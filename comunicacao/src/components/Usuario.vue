f91b4e682a187c6f8c70d40359865796a78ffd79
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-02-02 10:27:17 -0300

    finalização de comunicação entre componentes

--- comunicacao/src/components/Usuario.vue
+++ comunicacao/src/components/Usuario.vue
@@ -1,40 +1,68 @@
 <template>
-    <div class="container">
-        <h1>Componente Usuário</h1>
-        <p>Esse é um componente muito legal!</p>
-        <hr>
-        <div class="componentes">
-            <app-usuario-info />
-            <app-usuario-editar />
-        </div>
+  <div class="container">
+    <h1>Componente Usuário</h1>
+    <p>Esse é um componente muito legal!</p>
+    <p>
+      Meu nome é
+      <strong>{{nome}}</strong>
+      e tenho idade de
+      <strong>{{idade}}</strong>
+      anos
+    </p>
+    <button @click="alterarNome">Alterar</button>
+    <hr />
+    <div class="componentes">
+      <app-usuario-info
+        :nome="nome"
+        :reiniciarFn="reiniciarNome"
+        :idade="idade"
+        @nomeMudou="nome = $event.nomeNovo"
+      />
+      <!-- @idadeMudou="idade = $event" -->
+      <app-usuario-editar :idade="idade" />
     </div>
+  </div>
 </template>
 
 <script>
-import AppUsuarioInfo from './UsuarioInfo'
-import AppUsuarioEditar from './UsuarioEditar'
+import AppUsuarioInfo from "./UsuarioInfo";
+import AppUsuarioEditar from "./UsuarioEditar";
 
 export default {
-    components: { AppUsuarioInfo, AppUsuarioEditar }
-}
+  data() {
+    return {
+      nome: "pedro",
+      idade: 20
+    };
+  },
+  components: { AppUsuarioInfo, AppUsuarioEditar },
+  methods: {
+    alterarNome() {
+      this.nome = "ana";
+    },
+    reiniciarNome() {
+      this.nome = "Pedro";
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .container {
-        background-color: #27363b;
-        color: #fff;
-        padding: 10px;
-    }
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
 
-    .container hr {
-        margin: 20px 10px;
-    }
+.container hr {
+  margin: 20px 10px;
+}
 
-    .componentes {
-        display: flex;
-    }
+.componentes {
+  display: flex;
+}
 
-    .componentes > * {
-        margin: 10px;
-    }
+.componentes > * {
+  margin: 10px;
+}
 </style>
--- comunicacao/src/components/UsuarioInfo.vue
+++ comunicacao/src/components/UsuarioInfo.vue
@@ -1,20 +1,71 @@
 <template>
-    <div class="componente">
-        <h2>As Informações de Usuário</h2>
-        <p>Vários detalhes...</p>
-    </div>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do usuário:
+      <strong>{{nome}}</strong>
+    </p>
+    <p>
+      Idade do usuário:
+      <strong>{{idade}}</strong>
+    </p>
+    <p>
+      Nome do usuário invertido:
+      <strong>{{inverterNome()}}</strong>
+    </p>
+    <button @click="reiniciarNome">Reiniciar Nome</button>
+    <button @click="reiniciarFn()">Reiniciar Nome (Callback)</button>
+  </div>
 </template>
 
+<!-- Comunicação direta de componentes -->
 <script>
+import barramento from "@/barramento";
+
 export default {
-    
-}
+  //   props: ["nome"],
+  props: {
+    nome: {
+      type: String,
+      required: true,
+      default: "Anônimo"
+    },
+    idade: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    reiniciarFn: Function
+  },
+  methods: {
+    inverterNome() {
+      return this.nome
+        .split("")
+        .reverse()
+        .join("");
+    },
+    reiniciarNome() {
+      const nomeAntigo = this.nome;
+      this.nome = "Pedro";
+      this.$emit("nomeMudou", {
+        nomeNovo: this.nome,
+        nomeAntigo
+      });
+    }
+  },
+  created() {
+    barramento.quandoIdadeMudar(idade => {
+      this.idade = idade;
+    });
+  }
+};
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>
