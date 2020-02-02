7cd87a619603a01aa3d15f9e08fbd3b56d23c387
devleandrodias <leandro.dias_profissional@outlook.com.br>
2020-02-02 13:59:55 -0300

    finalização de gerenciamento de estado com vuex

--- vuex/src/components/Resumo.vue
+++ vuex/src/components/Resumo.vue
@@ -1,40 +1,38 @@
 <template>
-    <Painel titulo="Resumo" roxo>
-        <div class="resumo">
-            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
-            <hr>
-            <button>Finalizar!</button>
-        </div>
-    </Painel>
+  <Painel titulo="Resumo" roxo>
+    <div class="resumo">
+      <span>
+        Total:
+        <strong>{{ total | dinheiro }}</strong>
+      </span>
+      <hr />
+      <button>Finalizar!</button>
+    </div>
+  </Painel>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-    computed: {
-        total() {
-            return this.produtos.map(p => p.quantidade * p.preco)
-                .reduce((total, atual) => total + atual, 0)
-        }
-    },
-    data() {
-        return {
-            produtos: [
-                { nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-                { nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-                { nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-            ]
-        }
-    }
-}
+  computed: mapGetters("carrinho", {
+    total: "valorTotal"
+  })
+  //   computed: mapGetters(["valorTotal"])
+  //   computed: {
+  //     total() {
+  //       return this.$store.getters.valorTotal;
+  //     }
+  //   }
+};
 </script>
 
 <style>
-    table {
-        width: 100%;
-    }
+table {
+  width: 100%;
+}
 
-    td {
-        border-top: 1px solid #EEE;
-        width: 33%;
-    }
+td {
+  border-top: 1px solid #eee;
+  width: 33%;
+}
 </style>
